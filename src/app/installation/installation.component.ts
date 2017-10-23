04ad08a5b73fc494472d7628aef9aa2f5befd281
itswadesh <itswadesh@gmail.com>
2017-10-23 21:48:15 +0530

    updated installation instruction for deployment

--- src/app/installation/installation.component.ts
+++ src/app/installation/installation.component.ts
@@ -8,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class InstallationComponent implements OnInit {
   mongo: String;
   cli: String;
+  dist: String;
   constructor() { }
 
   ngOnInit() {
     this.mongo = `C:/Program Files/MongoDB/Server/3.2/bin/mongod.exe`;
     this.cli = `npm i
 npm run dev`;
+    this.dist = `npm run prod`;
   }
 
 }
